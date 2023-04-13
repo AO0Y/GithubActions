db04d95786747d700cbb3565fa84db662af47ec2
AO0Y <112539472+AO0Y@users.noreply.github.com>
2023-04-13 10:48:19 +0800

    Update UnitTests.cs
    fix the power test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -107,7 +107,7 @@ namespace GithubActionsLab
         public void Power_Valid()
         {
             Assert.AreEqual(1, Program.Power("1", "2"));
-            Assert.AreEqual(6, Program.Power("3", "2"));
+            Assert.AreEqual(9, Program.Power("3", "2"));
             Assert.AreEqual(125, Program.Power("5", "3"));
         }
 
